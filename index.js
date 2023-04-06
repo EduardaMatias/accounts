fe881ad0cbe7d95e619c56b0f875a42e9c51ae02
EduardaMatias <evcm2003@gmail.com>
2023-04-06 16:27:49 -0300

    Criando função de inicialização do sistema

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-import inquirer from 'inquirer'
-import chalk from 'chalk'
+import inquirer from 'inquirer';
+import chalk from 'chalk';
 
-import fs from 'fs'
+import fs from 'fs';
 
-console.log('ok')
+const options = [
+  'Criar conta',
+  'Consultar saldo',
+  'Depositar',
+  'Sacar',
+  'Sair',
+];
+
+(function operation() {
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'action',
+        message: 'O que você deseja fazer?',
+        choices: options,
+      },
+    ])
+    .then(() => {})
+    .catch((err) => console.log(err));
+})();
+
+console.log('ok');
