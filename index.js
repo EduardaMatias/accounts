0161f29cfb6d9a48d7adc134df465e035d283b67
EduardaMatias <evcm2003@gmail.com>
2023-04-06 23:58:16 -0300

    Implementando a função de deposito

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function operation() {
           console.log('Consultar saldo');
           break;
         case 'Depositar':
-          console.log('Depositar');
+          deposit();
           break;
         case 'Sacar':
           console.log('Sacar');
@@ -93,4 +93,83 @@ function buildAccount() {
       operation();
     })
     .catch((err) => console.log(err));
+}
+
+function deposit() {
+  inquirer
+    .prompt([
+      {
+        name: 'accountName',
+        message: 'Qual o nome da sua conta?',
+      },
+    ])
+    .then((answer) => {
+      const accountName = answer['accountName'];
+
+      if (!checkAccount(accountName)) {
+        return deposit();
+      }
+
+      inquirer
+        .prompt([
+          {
+            name: 'amount',
+            message: 'Quanto você deseja depositar?',
+          },
+        ])
+        .then((answer) => {
+          const amount = answer['amount'];
+
+          addAmount(accountName, amount);
+
+          operation();
+        })
+        .catch((err) => console.log(err));
+    })
+    .catch((err) => console.log(err));
+}
+
+function addAmount(accountName, amount) {
+  const accountData = getAccount(accountName);
+
+  if (!amount) {
+    console.log(
+      chalk.bgRed.black('Ocorreu um erro, tente novamente mais tarde!')
+    );
+    return;
+  }
+
+  accountData.balance = parseFloat(amount) + parseFloat(accountData.balance);
+
+  fs.writeFileSync(
+    `accounts/${accountName}.json`,
+    JSON.stringify(accountData),
+    function (err) {
+      console.log(err);
+    }
+  );
+
+  console.log(
+    chalk.green(`Foi depositado o valor de R$${amount} na sua conta!`)
+  );
+}
+
+function checkAccount(accountName) {
+  if (!fs.existsSync(`accounts/${accountName}.json`)) {
+    console.log(
+      chalk.bgRed.black('Esta conta não existe, escolha outro nome!')
+    );
+    return false;
+  }
+
+  return true;
+}
+
+function getAccount(accountName) {
+  const accountJSON = fs.readFileSync(`accounts/${accountName}.json`, {
+    encoding: 'utf8',
+    flag: 'r',
+  });
+
+  return JSON.parse(accountJSON);
 }
