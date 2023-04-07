a9f8c4670d965c2a46ac03068d10ccc5b3cddcc9
EduardaMatias <evcm2003@gmail.com>
2023-04-06 23:26:12 -0300

    Implementando função de criar conta

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ const menuOptions = [
   'Sair',
 ];
 
-(function operation() {
+operation();
+
+function operation() {
   inquirer
     .prompt([
       {
@@ -21,8 +23,61 @@ const menuOptions = [
         choices: menuOptions,
       },
     ])
-    .then(() => {})
-    .catch((err) => console.log(err));
-})();
+    .then((answer) => {
+      const action = answer['action'];
 
-console.log('ok');
+      switch (action) {
+        case 'Criar conta':
+          createAccount();
+          break;
+      }
+    })
+    .catch((err) => console.log(err));
+}
+
+function createAccount() {
+  console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco!'));
+  console.log(chalk.green('Defina as opções da sua conta a seguir'));
+
+  buildAccount();
+}
+
+function buildAccount() {
+  inquirer
+    .prompt([
+      {
+        name: 'accountName',
+        message: 'Digite um nome para sua conta:',
+      },
+    ])
+    .then((answer) => {
+      const accountName = answer['accountName'];
+      console.info(accountName);
+
+      if (!fs.existsSync('accounts')) {
+        fs.mkdirSync('accounts');
+      }
+
+      if (fs.existsSync(`accounts/${accountName}.json`)) {
+        console.log(
+          chalk.bgRed.black('Esta conta já existe, escolha outro nome!')
+        );
+
+        buildAccount();
+        return;
+      }
+
+      fs.writeFileSync(
+        `accounts/${accountName}.json`,
+        '{"balance": 0}',
+        function (err) {
+          console.log(err);
+        }
+      );
+
+      console.log(chalk.green('Parabéns, sua conta foi criada!'));
+
+      operation();
+    })
+    .catch((err) => console.log(err));
+}
