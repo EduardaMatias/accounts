04242b6b1a445c8151451d80236d8fe92cb06535
EduardaMatias <evcm2003@gmail.com>
2023-04-07 00:22:34 -0300

    Implementando função de saque

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ function operation() {
           deposit();
           break;
         case 'Sacar':
-          console.log('Sacar');
+          withdraw();
           break;
         case 'Sair':
           console.log(chalk.bgBlue.black('Obrigada por usar o Accounts!'));
@@ -180,6 +180,70 @@ function getAccountBalance() {
       operation();
     })
     .catch((err) => console.log(err));
+}
+
+function withdraw() {
+  inquirer
+    .prompt([
+      {
+        name: 'accountName',
+        message: 'Qual o nome da sua conta?',
+      },
+    ])
+    .then((answer) => {
+      const accountName = answer['accountName'];
+
+      if (!checkAccount(accountName)) {
+        return withdraw();
+      }
+
+      inquirer
+        .prompt([
+          {
+            name: 'amount',
+            message: 'Quanto você deseja sacar?',
+          },
+        ])
+        .then((answer) => {
+          const amount = answer['amount'];
+
+          removeAmount(accountName, amount);
+        })
+        .catch((err) => console.log(err));
+    })
+    .catch((err) => console.log(err));
+}
+
+function removeAmount(accountName, amount) {
+  const accountData = getAccount(accountName);
+
+  if (!amount) {
+    console.log(
+      chalk.bgRed.black('Ocorreu um erro, tente novamente mais tarde!')
+    );
+    return withdraw();
+  }
+
+  if (accountData.balance < amount) {
+    console.log(chalk.bgRed.black('Valor indisponível no momento!'));
+    return withdraw();
+  }
+
+  accountData.balance = parseFloat(accountData.balance) - parseFloat(amount);
+
+  fs.writeFileSync(
+    `accounts/${accountName}.json`,
+    JSON.stringify(accountData),
+    function (err) {
+      console.log(err);
+    }
+  );
+
+  console.log(
+    chalk.green(`Foi realizado um saque no valor de R$${amount} da sua conta!`)
+  );
+
+  operation();
 }
 
 //utils
